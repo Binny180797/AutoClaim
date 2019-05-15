25b2adcab18e28d4d56a6abf0d3c76537348404e
Binnish sai K <binnishsaikalimidi@gmail.com>
2019-05-15 14:31:08 +0530

    gjhgjhjkl

---
+++ AutoClaim/Controllers/PolicyController.cs
@@ -0,0 +1,48 @@
+﻿using AutoClaim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoClaim.Controllers
+{
+    public class PolicyController : Controller
+    {
+        private ApplicationDbContext dbContext = null;
+        public PolicyController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        // GET: Policy
+        public ActionResult Index()
+        {
+           
+            return View();
+
+
+        }
+        [HttpGet]
+        public ActionResult Registration( )
+        {
+           
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(Policy policy)
+        {
+
+
+
+            return View();
+        }
+
+    }
+}
